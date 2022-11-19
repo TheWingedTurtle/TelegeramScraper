fbb09869a500e8a9930b3736ee4fa306f31c2040
abhinav <abhinav0225@gmail.com>
2022-11-20 00:28:31 +0530

    Refactor single script file
    1. Add logging instead of sysout
    2. Refactor rabbitMQ for non blocking connection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import asyncio
-import sys
+import logging
 from configparser import ConfigParser
 
 import aio_pika
@@ -7,18 +7,59 @@ import aiofiles
 import telethon
 from telethon import events
 from telethon.errors import SessionPasswordNeededError
-from telethon.tl.types import PeerChannel
 
 cp = ConfigParser()
 cp.read('config.ini')
 
 mq_config = {}
 
+logging.basicConfig(filename='tgbot.log', filemode='a', format='%(asctime)s - %(message)s', level=logging.INFO)
 
-async def init_rmq():
+
+class UserInfo:
+    def __init__(self):
+        self.username = \
+            cp['Telegram']['username'] if cp['Telegram']['username'] != 'unset' else None
+        self.api_id = \
+            cp['Telegram']['api_id'] if cp['Telegram']['api_id'] != 'unset' else None
+        self.api_hash = \
+            cp['Telegram']['api_hash'] if cp['Telegram']['api_hash'] != 'unset' else None
+        self.phone = \
+            cp['Telegram']['phone'] if cp['Telegram']['phone'] != 'unset' else None
+
+
+async def init():
+    user = UserInfo()
+    if user.username is None or user.phone is None or user.api_id is None or user.api_hash is None:
+        raise Exception("Please fill the config.ini")
+    client = telethon.TelegramClient(user.username, int(user.api_id), user.api_hash)
+    await client.start()
+    if not await client.is_user_authorized():
+        await client.send_code_request(user.phone)
+        try:
+            logging.info("Using Phone number: " + user.phone)
+            await client.sign_in(user.phone, input('Enter the code: '))
+        except SessionPasswordNeededError:
+            await client.sign_in(password=input('Password: '))
+    return user, client
+
+
+class ChannelsInfo:
+    def __init__(self):
+        self.channel_ids = []
+        channels = cp['Telegram']['channel_ids']
+        if channels == 'unset':
+            raise Exception("Please fill the details in config.ini")
+        for w in channels.split(' '):
+            if w.isdigit():
+                self.channel_ids.append(int(w))
+
+
+async def init_rmq(ext_loop):
     mq_config['enabled'] = True
-    mq_connection = await aio_pika.connect_robust()
-    mq_connection.close_callbacks.add(mq_connection.reconnect)
+    mq_connection = await aio_pika.connect_robust(
+        "amqp://guest:guest@127.0.0.1/", loop=ext_loop, timeout=60
+    )
     mq_config['connection']: aio_pika.abc.AbstractRobustConnection = mq_connection
     mq_config['routing_key'] = cp['RabbitMQ']['queue_route_key']
     channel = await mq_connection.channel()
@@ -30,125 +71,49 @@ async def init_rmq():
     mq_config['queue'] = queue
 
 
-async def main():
-    user, client = await init()
-    channels = ChannelsInfo()
-    channel_tasks = []
-    if cp['RabbitMQ']['enabled'] == 'true':
-        await init_rmq()
-    for channel_id in channels.channel_ids:
-        channel_tasks.append(asyncio.create_task(async_read_new_messages(channel_id, client)))
-        # channel_tasks.append(asyncio.create_task(read_multiple_channels_all_messages([channel_id], client)))
-    [await task for task in channel_tasks]
-
-
-async def init():
-    user = UserInfo()
-    client = telethon.TelegramClient(user.username, int(user.api_id), user.api_hash)
-    await client.start()
-    if not await client.is_user_authorized():
-        await client.send_code_request(user.phone)
-        try:
-            print("Using Phone number: " + user.phone)
-            await client.sign_in(user.phone, input('Enter the code: '))
-        except SessionPasswordNeededError:
-            await client.sign_in(password=input('Password: '))
-    return user, client
-
-
-class UserInfo:
-    def __init__(self):
-        self.username = \
-            cp['Telegram']['username'] if cp['Telegram']['username'] != 'unset' else input('Enter Telegram Username: ')
-        self.api_id = \
-            cp['Telegram']['api_id'] if cp['Telegram']['api_id'] != 'unset' else input('Enter api id: ')
-        self.api_hash = \
-            cp['Telegram']['api_hash'] if cp['Telegram']['api_hash'] != 'unset' else input('Enter api hash: ')
-        self.phone = \
-            cp['Telegram']['phone'] if cp['Telegram']['phone'] != 'unset' \
-                else input('Enter phone number (including country code (+91 for India): ')
-
-
-class ChannelsInfo:
-    def __init__(self):
-        self.channel_ids = []
-        channels = cp['Telegram']['channel_ids']
-        if channels == 'unset':
-            channels = input('Enter space separated channel ids: ')
-        for w in channels.split(' '):
-            if w.isdigit():
-                self.channel_ids.append(int(w))
-
-
-async def dump(messages, prefix: str):
-    if mq_config['enabled']:
-        exchange: aio_pika.abc.AbstractRobustExchange = mq_config['exchange']
-        for message in messages:
-            if 'message' in message.to_dict():
-                await exchange.publish(
-                    aio_pika.Message(body=message.to_dict()['message'].encode()),
-                    mq_config['routing_key']
-                )
-    with open(prefix + '.' 'messages.json', 'w') as outfile:
-        for message in messages:
-            if 'message' in message.to_dict():
-                outfile.write(message.to_dict()['message'])
-                outfile.write('\n')
-
-
 async def dump_new_messages(message, channel_id):
-    if mq_config['enabled']:
-        exchange: aio_pika.abc.AbstractRobustExchange = mq_config['exchange']
-        await exchange.publish(
-            aio_pika.Message(body=message.to_dict()['message'].encode()),
-            mq_config['routing_key'])
-    else:
-        async with aiofiles.open(str(channel_id) + '.' 'messages.json', 'w') as f:
+    try:
+        if cp['RabbitMQ']['enabled']:
+            await init_rmq(asyncio.get_running_loop())
+            exchange: aio_pika.abc.AbstractRobustExchange = mq_config['exchange']
             if 'message' in message.to_dict():
-                await f.write(message.to_dict()['message'] + '\n')
+                message_body = message.to_dict()['message']
+                logging.info("Sending message: " + str(message_body))
+                await exchange.publish(
+                    aio_pika.Message(body=message_body.encode()),
+                    mq_config['routing_key'])
+        else:
+            async with aiofiles.open(str(channel_id) + '.' 'messages.json', 'w') as f:
+                if 'message' in message.to_dict():
+                    await f.write(message.to_dict()['message'] + '\n')
+    except Exception as e:
+        logging.info("Exception occurred:" + str(e))
+    finally:
+        await mq_config['connection'].close()
 
 
 async def async_read_new_messages(channel_id: int, client: telethon.TelegramClient):
     client.add_event_handler(lambda event: dump_new_messages(event.message, channel_id),
                              events.NewMessage(chats=channel_id))
-    print('Async task to read new messages scheduled.')
+    logging.info('Async task to read new messages scheduled.')
     return await client.run_until_disconnected()
 
 
-async def read_all_channel_messages(channel: telethon.types.InputPeerChannel, client: telethon.TelegramClient):
-    last_offset = -1
-    limit = 100
-    while True:
-        # First run
-        if last_offset == -1:
-            messages = await client.get_messages(entity=channel, reverse=False, limit=100)
-        else:
-            messages = await client.get_messages(entity=channel, reverse=False, max_id=last_offset, limit=limit)
-
-        if messages is None or len(messages) == 0:
-            break
-
-        await dump(messages, str(channel.channel_id))
-        last_offset = messages[len(messages) - 1].id
-
-
-async def read_multiple_channels_all_messages(channel_ids: list, client: telethon.TelegramClient):
+async def main():
+    user, client = await init()
+    channels = ChannelsInfo()
     channel_tasks = []
-    for channel_id in channel_ids:
-        entity = PeerChannel(int(channel_id))
-        channel = await client.get_input_entity(entity)
-        channel_tasks.append(asyncio.create_task(read_all_channel_messages(channel, client)))
-        print('Async task to dump messages scheduled. Messages will be dumped at ' + str(channel_id) + '.messages.json')
+    for channel_id in channels.channel_ids:
+        channel_tasks.append(asyncio.create_task(async_read_new_messages(channel_id, client)))
     [await task for task in channel_tasks]
 
 
 if __name__ == '__main__':
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     try:
-        asyncio.run(main())
+        loop.run_until_complete(main())
     except KeyboardInterrupt:
-        print("Keyboard Interrupt. Exiting..")
-        sys.exit(0)
-    finally:
-        connection: aio_pika.abc.AbstractRobustConnection = mq_config['connection']
-        if not connection.is_closed:
-            asyncio.run(connection.close())
+        logging.info("Keyboard Interrupt. Exiting..")
+        if loop.is_running():
+            loop.stop()
